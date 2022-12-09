3660aa43ef5a4cb323638597cbc8b6cb4a68afe1
JEONG HYUN SEOK <hyunseok.jeong@annotation-ai.com>
2022-12-10 01:44:32 +0900

    feat: query-build

--- query-build/main.go
+++ query-build/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
-	"log"
+	"fmt"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/sqlitedialect"
@@ -46,8 +46,10 @@ func createTable() {
 
 func insertUsers() {
 	for _, user := range users {
-		res, err := db.NewInsert().Model(&user).Exec(context.Background())
-		log.Println("res:", res, "err:", err)
+		_, err := db.NewInsert().Model(&user).Exec(context.Background())
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -58,12 +60,26 @@ func prepareDB() {
 }
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	prepareDB()
 
+	// Query build 방법 1
 	query := db.NewSelect().Model((*User)(nil))
-	log.Println(query)
-	query.Where("name =?", "kim")
-	log.Println(query)
+	fmt.Printf("memory address %p, query: %v\n", query, query)
 
+	query = query.Where("name =?", "kim") // Where() 메서드의 결과를 왼쪽으로 대입한다
+	fmt.Printf("memory address %p, query: %v\n", query, query)
+
+	// Query build 방법 2
+	query = db.NewSelect().Model((*User)(nil))
+	fmt.Printf("memory address %p, query: %v\n", query, query)
+
+	query.Where("name =?", "kim") // 대입을 시키지 않고 단순히 Where() 만을 실행한다.
+	fmt.Printf("memory address %p, query: %v\n", query, query)
 }
+
+// 결과를 보면
+// 1. query를 추가해주어도 두 경우 모두 메모리 주소가 바뀌지 않고, 생성된 query string 도 동일함을 알 수 있다.
+// memory address 0x140002e8000, query: SELECT "u"."id", "u"."name" FROM "users" AS "u"
+// memory address 0x140002e8000, query: SELECT "u"."id", "u"."name" FROM "users" AS "u" WHERE (name ='kim')
+// memory address 0x140002e81e0, query: SELECT "u"."id", "u"."name" FROM "users" AS "u"
+// memory address 0x140002e81e0, query: SELECT "u"."id", "u"."name" FROM "users" AS "u" WHERE (name ='kim')
